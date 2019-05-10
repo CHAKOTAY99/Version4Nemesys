b6a2a66b7cff21b4bc73bb82d5e6057b0aa25cbe
CHAKOTAY99 <40712736+CHAKOTAY99@users.noreply.github.com>
2019-05-10 17:17:45 +0200

    Added Models

--- Version4Nemesys/Data/ApplicationDbContext.cs
+++ Version4Nemesys/Data/ApplicationDbContext.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Version4Nemesys.Models;
 
 namespace Version4Nemesys.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext()
+        {
+
+        }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<HazardModel> Hazard { get; set; }
+        public DbSet<InvestigationModel> Investigations { get; set; }
+        public DbSet<ReportModel> Reports { get; set; }
+        public DbSet<PhotoModel> Photos { get; set; }
     }
 }
---
+++ Version4Nemesys/Data/Migrations/20190510151536_NewModels.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Version4Nemesys.Data.Migrations
+{
+    public partial class NewModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Hazard",
+                columns: table => new
+                {
+                    HazardID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    HazardName = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hazard", x => x.HazardID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Investigations",
+                columns: table => new
+                {
+                    InvestigationID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ActionDate = table.Column<DateTime>(nullable: false),
+                    InvestigationDescription = table.Column<string>(nullable: false),
+                    UserID = table.Column<int>(nullable: false),
+                    States = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Investigations", x => x.InvestigationID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    PhotoID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    PhotoName = table.Column<string>(nullable: false),
+                    PhotoPath = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.PhotoID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reports",
+                columns: table => new
+                {
+                    ReportID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    EventDate = table.Column<DateTime>(nullable: false),
+                    ReportDate = table.Column<DateTime>(nullable: false),
+                    HazardID = table.Column<int>(nullable: false),
+                    UserID = table.Column<int>(nullable: false),
+                    EventLocation = table.Column<string>(nullable: false),
+                    EventDescription = table.Column<string>(nullable: false),
+                    PhotoID = table.Column<int>(nullable: false),
+                    ReportUpvotes = table.Column<int>(nullable: false),
+                    States = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reports", x => x.ReportID);
+                    table.ForeignKey(
+                        name: "FK_Reports_Hazard_HazardID",
+                        column: x => x.HazardID,
+                        principalTable: "Hazard",
+                        principalColumn: "HazardID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reports_Photos_PhotoID",
+                        column: x => x.PhotoID,
+                        principalTable: "Photos",
+                        principalColumn: "PhotoID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reports_HazardID",
+                table: "Reports",
+                column: "HazardID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reports_PhotoID",
+                table: "Reports",
+                column: "PhotoID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Investigations");
+
+            migrationBuilder.DropTable(
+                name: "Reports");
+
+            migrationBuilder.DropTable(
+                name: "Hazard");
+
+            migrationBuilder.DropTable(
+                name: "Photos");
+        }
+    }
+}
