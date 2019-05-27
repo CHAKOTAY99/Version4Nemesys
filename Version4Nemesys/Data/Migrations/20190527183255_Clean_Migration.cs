43b57edccbd547b642a560d50705a07fe6a845f1
zahramark <47721432+zahramark@users.noreply.github.com>
2019-05-27 20:35:41 +0200

    clean migration mark

---
+++ Version4Nemesys/Data/Migrations/20190527183255_Clean_Migration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Version4Nemesys.Data.Migrations
+{
+    public partial class Clean_Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
